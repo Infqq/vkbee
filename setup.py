83ae4a486fe408fbac752d064fd779adc866cf4f
YamkaFox <46236371+YamkaFox@users.noreply.github.com>
2020-02-04 13:59:26 +0800

    1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r",encoding='utf-8') as fh:
 
 setuptools.setup(
     name="vkbee",
-    version="1.3",
+    version="1.4",
     author="YamkaFox",
     author_email="cryptoyamafox@gmail.com",
     description="Simple Async VKLibrary faster than vk_api",
