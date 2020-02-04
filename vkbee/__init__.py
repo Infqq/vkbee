5604c00afb4c4dbf526a242dc55e75058197e874
YamkaFox <46236371+YamkaFox@users.noreply.github.com>
2020-02-04 13:59:05 +0800

    [ULP] UserLongPoll Beta 1.0

--- vkbee/__init__.py
+++ vkbee/__init__.py
@@ -131,3 +131,69 @@ class BotLongpoll:
             for event in await self.get_events():
                 yield event
 
+
+class UserLongpoll:
+    def __init__(self,vk,wait=10):
+        self.wait = wait
+        self.vk = vk
+
+        self.method_url = "https://api.vk.com/method/messages.getLongPollServer"
+
+        self.url = None
+        self.key = None
+        self.server = None
+        self.ts = None
+        self.start_time = time.time()
+        self.request_count = 0
+
+    async def update_server(self,update_ts=True):
+        data = {"lp_version": 3}
+        # r = await self.s.post(self.method_url, data=data)
+        r = await self.vk.call("messages.getLongPollServer", data=data)
+        self.key = r["response"]["key"]
+        self.server = r["response"]["server"]
+        self.url = self.server
+
+        if update_ts:
+            self.ts = r["response"]["ts"]
+
+    async def get_events(self):
+        params = {
+            "act": "a_check",
+            "key": self.key,
+            "ts": self.ts,
+            "wait": self.wait,
+            "mode": 234
+        }
+        response = await self.vk.s.get(self.url, params=params)
+
+        self.request_count += 1
+        # work_time = time.time() - self.start_time
+        # avr_time = work_time / self.request_count
+        # speed = self.request_count / work_time
+        print(f'All Get Events - {self.request_count}')
+        # print(f'Work Time - {work_time}')
+        # print(f'Requests in Second - {speed}')
+        # print(f'Average Time - {avr_time}')
+
+        response = await response.json()
+
+        if "failed" not in response:
+            return response["updates"]
+
+        elif response["failed"] == 1:
+            self.ts = response["ts"]
+
+        elif response["failed"] == 2:
+            self.update_server()(update_ts=False)
+
+        elif response["failed"] == 3:
+            self.update_server()
+
+        return []
+
+    async def events(self):
+        await self.update_server()
+        while True:
+            for event in await self.get_events():
+                yield event
