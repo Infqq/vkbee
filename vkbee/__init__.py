da9c6f0d8056f6ac6a5b2e7b7e99dcefa5699940
YamkaFox <46236371+YamkaFox@users.noreply.github.com>
2020-02-05 14:23:07 +0800

    Update __init__.py

--- vkbee/__init__.py
+++ vkbee/__init__.py
@@ -130,7 +130,9 @@ class BotLongpoll:
     async def events(self):
         await self.update_server(self)
         while True:
+            await self.update_server()
             for event in await self.get_events():
+                await self.update_server()
                 yield event
 
 
