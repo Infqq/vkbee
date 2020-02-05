50aaa5d553eef49065885dda35578678a6c3ac0f
YamkaFox <46236371+YamkaFox@users.noreply.github.com>
2020-02-05 14:27:19 +0800

    остановите этот бит витя танцует витя не спит астанавите астаниве вите вите нада выйти астанавити астанавити вити вити нада выйти

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r",encoding='utf-8') as fh:
 
 setuptools.setup(
     name="vkbee",
-    version="1.5",
+    version="1.6",
     authors=["YamkaFox","sergeyfilippov1"],
     author_email="cryptoyamafox@gmail.com",
     description="Simple Async VKLibrary faster than vk_api",
