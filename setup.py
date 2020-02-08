fd6f68168bd7861ef72775304f469e07e5ab7c72
YamkaFox <46236371+YamkaFox@users.noreply.github.com>
2020-02-08 21:31:06 +0800

    PyPi : Deployed as YamkaFox version 2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r",encoding='utf-8') as fh:
 
 setuptools.setup(
     name="vkbee",
-    version="1.6",
+    version="2.0",
     authors=["YamkaFox","sergeyfilippov1"],
     author_email="cryptoyamafox@gmail.com",
     description="Simple Async VKLibrary faster than vk_api",
