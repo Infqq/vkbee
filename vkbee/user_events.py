c5228201b27414409cfed53b2ec60957ec3d60a9
Kirill Eremenko <68491167+cugadese@users.noreply.github.com>
2020-08-09 17:07:27 +0800

    Update user_events.py

--- vkbee/user_events.py
+++ vkbee/user_events.py
@@ -18,14 +18,28 @@ def event_flags_get(number)
     if number == 9:
         return 'FRIEND_OFFLINE'
     if number == 10:
-        return 'RESET_MESSAGE_FLAGS2'
+        return 'RESET_MESSAGE_FLAGS'
     if number == 11:
-        return 'REPLACE_MESSAGE_FLAGS2'
+        return 'REPLACE_MESSAGE_FLAGS'
     if number == 12:
-        return 'SET_MESSAGE_FLAGS2'
+        return 'SET_MESSAGE_FLAGS'
     if number == 13:
         return 'DELETE_MESSAGE'
     if number == 14:
         return 'UNDO_DELETE_MESSAGE'
     if number == 20:
         return 'MAJOR_ID_CHANGED'
+    if number == 21:
+        return 'MINOR_ID_CHANGED'
+    if number == 51:
+        return 'SOSTAV_OR_THEME_CHANGED' 
+        # WTF
+    if number == 52:
+        return 'CHANGE_CHAT_INFO'
+    if number == 61:
+        return 'USER_TYPING'
+    if number == 62:
+        return 'USER_TYPING'
+    if number == 63:
+        return 'USERS_TYPING'
+    if number == 64:
